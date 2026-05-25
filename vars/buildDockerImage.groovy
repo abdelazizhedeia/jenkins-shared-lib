b3269dc3af4dd36906b67c3140333a2c1d417756
Abdelaziz Mohamed <61994281+abdelazizhedeia@users.noreply.github.com>
2026-05-26 02:17:14 +0300

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,1 +1,12 @@
+def call(String imageName, String tag) {
 
+    def fullImage = "${imageName}:${tag}"
+
+    echo "Building Docker image: ${fullImage}"
+
+    sh """
+        docker build -t ${fullImage} .
+    """
+
+    echo "Docker build done"
+}
